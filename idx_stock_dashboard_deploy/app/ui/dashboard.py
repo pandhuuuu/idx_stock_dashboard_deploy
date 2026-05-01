ddcd823cd9a73d25b592c7de82f9414425e36d23
pandhuuuu <149009188+pandhuuuu@users.noreply.github.com>
2026-05-01 19:15:45 +0700

    Update dashboard.py

--- idx_stock_dashboard_deploy/app/ui/dashboard.py
+++ idx_stock_dashboard_deploy/app/ui/dashboard.py
@@ -379,7 +379,7 @@ if mode == "Auto IDX Full":
 else:
     tickers_source = DEFAULT_TICKERS
 
-tickers_input    = st.sidebar.text_area("Kode Saham (100 Ticker Default)", ",".join(tickers_source[:100]), key="ticker_scan_v3")
+tickers_input    = st.sidebar.text_area("Kode Saham (100 Ticker Default)", ",".join(tickers_source[:30]), key="ticker_scan_v3")
 period           = st.sidebar.selectbox("Period",   ["3mo", "6mo", "9mo", "1y", "2y", "3y", "5y", "10y"], index=3)
 interval         = st.sidebar.selectbox("Interval", ["1d", "1wk", "1mo"], index=0)
 run_button       = st.sidebar.button("🚀 Scan Sekarang")
