e78f6e2fec8bd148b29be1ada7277ba9cb89d82b
pandhuuuu <149009188+pandhuuuu@users.noreply.github.com>
2026-05-03 05:10:29 +0700

    Update streamlit_app.py

--- idx_stock_dashboard_deploy/streamlit_app.py
+++ idx_stock_dashboard_deploy/streamlit_app.py
@@ -19,7 +19,7 @@ from idx_stock_monitor import (
     get_all_idx_tickers
 )
 
-st.set_page_config(page_title="IDX Stock Dashboard", layout="wide", page_icon="📊")
+st.set_page_config(page_title="MarketLens", layout="wide", page_icon="🧿")
 
 # ─────────────────────────────
 # CUSTOM CSS — PROFESSIONAL TRADING TERMINAL
